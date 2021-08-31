a9baecc3330c6c2978e9daa859bd69861e66d76c
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-08-31 21:27:45 +0100

    Added the repository and personal links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        this project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
+          target="_blank"
+        >
+          sara nascimento
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/saranascimento9/react-weather-app"
+          target="_blank"
+        >
+          open sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
