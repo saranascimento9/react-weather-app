5a6422fdfd00b6aa9343d216124258543a61b917
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-08-31 21:41:41 +0100

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        this project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          sara nascimento
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/saranascimento9/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          this project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            sara nascimento
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/saranascimento9/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
